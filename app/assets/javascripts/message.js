12ec6ab431abe744155a58f8816387b5fe0bcbde
delfin378 <type0bs02hd@yahoo.co.jp>
2019-12-11 20:31:02 +0900

    kansei

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,14 @@
 $(function(){
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
-    var image = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
                 <div class="upper-message">
                 <div class="upper-message__user-name">
                   ${message.user_name}
                 </div>
                 <div class="upper-message__date">
-                  ${message.date}
+                  ${message.created_at}
                 </div>
                 </div>
                 <div class="lower-message">
@@ -55,4 +55,30 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     })
   });
-});
+
+    var reloadMessages = function () {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        last_message_id = $('.message:last').data("message-id");
+        $.ajax({ 
+          url: "api/messages", 
+          type: 'get', 
+          dataType: 'json', 
+          data: {last_id: last_message_id} 
+        })
+
+        .done(function (messages) { 
+          console.log(messages)
+          var insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML += buildHTML(message);
+            $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+        })
+        .fail(function () {
+          console.log('自動更新に失敗しました');
+        });
+      }
+    };
+    setInterval(reloadMessages, 7000);
+  });
